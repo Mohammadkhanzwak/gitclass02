bc9665f3d6eb07a8a517fa435ed45a70b75cb1a9
syntaxTechnologies <mohammadkhanzwak150@gmail.com>
2025-02-20 21:36:15 -0800

    person2commit

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -2,7 +2,7 @@ public class test {
     public static void main(String[] args) {
 
         System.out.println("this is a new project");
-        System.out.println("this is a version 1");
+        System.out.println("this is a version 6 made by person 2");
 
         System.out.println("hi man");
 
